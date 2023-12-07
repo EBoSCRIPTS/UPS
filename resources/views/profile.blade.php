ee7f993cef60e7e40aaa58a64c2693abdf4b8b16
Martins <grazhanskis17@gmail.com>
2023-12-07 13:24:40 +0200

    Start updating profile page layout

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -9,26 +9,74 @@
 </head>
 
 <body>
+<div class="row">
     @include('components.sidebar')
-        <div class="grid">
+
         @if(isset(Auth::user()->email))
-            <div class="row">
-                <div class="col-5 col-md-3">
-                <h2> <img class="img-responsive float-start" src="data:image/png;base64,{{ base64_encode(Auth::user()->profile_picture) }}" alt="Profile Picture"> </h2>
+            <div class="col-lg-4">
+                <div class="card bg-light">
+                    <div class="card-body text-center">
+                        <img src="{{ asset($user->profile_picture) }}" alt="Profile Picture" class="rounded-circle mb-3" width="150" height="150">
+                        @if(isset($user->employee->department->name))
+                        <p class="text-muted">{{$user->employee->department->name}}</p>
+                        @endif
+                        <form action="mailto:{{$user->email}}" method="post">
+                            @csrf
+                            <button type="submit" class="btn btn-primary mt-3">Send email</button>
+                        </form>
+                    </div>
                 </div>
-                <div class="col-6 col-md-4">
-                <p class="fs-3">User: {{ Auth::user()->first_name }} {{ Auth::user()->last_name }}</p>
-                <p class="fs-3">Email: {{ Auth::user()->email }}</p>
-                <p class="fs-3">Phone Number: {{ Auth::user()->phone_number }}</p>
-                @if(Auth::user()->role_id == 1)
-                    <p class="fs-3">Role: SuperAdmin</p>
-                @endif
             </div>
+
+            <div class="col-lg-8">
+                <div class="card bg-light">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-sm-3">
+                                <p class="mb-0">Full name:</p>
+                            </div>
+                            <div class="col-sm-9">
+                                <p class="text-muted mb-0">{{$user->first_name}} {{$user->last_name}}</p>
+                            </div>
+                        </div>
+                        <hr class="hr"/>
+                        <div class="row">
+                            <div class="col-sm-3">
+                                <p class="mb-0">Email:</p>
+                            </div>
+                            <div class="col-sm-9">
+                                <p class="text-muted mb-0">{{$user->email}}</p>
+                            </div>
+                        </div>
+                        <hr class="hr">
+                        <div class="row">
+                            <div class="col-sm-3">
+                                <p class="mb-0">Phone number:</p>
+                            </div>
+                            <div class="col-sm-9">
+                                <p class="text-muted mb-0">{{$user->phone_number}}</p>
+                            </div>
+                        </div>
+                        <hr class="hr"/>
+
+                        <div class="row">
+                            <div class="col-sm-3">
+                                <p class="mb-0">Position:</p>
+                            </div>
+                            <div class="col-sm-9">
+                                @if(isset($user->employee->position))
+                                <p class="text-muted mb-0">{{$user->employee->position}}</p>
+                                @else
+                                <p class="text-muted mb-0">No position</p>
+                                @endif
+
+                            </div>
+                        </div>
+                </div>
             </div>
             @else
             {{redirect('/login')}}
         @endif
-        </div>
-    </div>
+</div>
 
 </body>
