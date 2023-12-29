e51d175566615c8c7c459991cc171d9ea7fac633
Martins <grazhanskis17@gmail.com>
2023-12-29 23:52:59 +0200

    Add DB backup schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -10,9 +10,10 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      */
-    protected function schedule(Schedule $schedule): void
+    protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('backup:clean')->daily()->at('1:00');
+        $schedule->command('backup:run')->daily()->at('03:00');
     }
 
     /**
